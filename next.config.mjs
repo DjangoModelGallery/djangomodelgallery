2538f6d6f4cddfc6f98ec9a16392d2f5a393d824
HR. Lee <matt14507@gmail.com>
2024-04-26 01:25:44 +0900

    💚 ci/cd: workflow 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_BASE_PATH,
+
   output: process.env.NEXT_PUBLIC_OUTPUT_MODE || "standalone",
 };
 
