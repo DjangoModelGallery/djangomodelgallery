c8a63ec3e4059364201370b9fdea4cb3b77c9e0d
HR. Lee <matt14507@gmail.com>
2024-04-26 01:13:02 +0900

    💚 ci/cd: 추가 수정 및 필요없는 파일 제거

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   output: process.env.NEXT_PUBLIC_OUTPUT_MODE || "standalone",
 };
 
